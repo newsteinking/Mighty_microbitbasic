f66c986d33723fa90257c73e52e0b9fd86a56f02
BuildTools <unconfigured@null.spigotmc.org>
2018-12-06 17:33:08 +0900

    dd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ release = 'DEV'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'sphinx.ext.m2r'
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,8 +48,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-#source_suffix = '.rst'
+#source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
